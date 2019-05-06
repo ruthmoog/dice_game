4e9ab51e49a4baa270c22c7b875aba69acacb66c
Ruth Baker <ruthmoog@gmail.com>
2019-05-06 13:20:03 +0100

    Add a method that rolls 2 dice

--- dice_game.rb
+++ dice_game.rb
@@ -5,8 +5,12 @@
 # Upgrade the dice game: now both I and my opponent roll two six-sided dice each.
 # Upgrade the game once more: now we play several rounds for each game. The first player to win two rounds wins the game. (Still with two dice each.)
 
-player1_score = rand(1..6)
-player2_score = rand(1..6)
+def roll
+  rand(1..6) + rand(1..6)
+end
+
+player1_score = roll
+player2_score = roll
 
 puts "p1: #{player1_score} | p2: #{player2_score}"
 if player1_score > player2_score
