09ff40c4e93b24533961db2736240543eb04284c
Ruth Baker <ruthmoog@gmail.com>
2019-05-06 15:16:52 +0100

    Run the game until a player scores 2 wins

--- dice_game.rb
+++ dice_game.rb
@@ -9,12 +9,18 @@ def roll
   rand(1..7) + rand(1..7)
 end
 
-player1_score = roll
-player2_score = roll
+player1_score = 0
+player2_score = 0
 
-puts "p1: #{player1_score} | p2: #{player2_score}"
-if player1_score > player2_score
-  puts "I win"
-else
-  puts "I lose"
+
+until player1_score == 2 || player2_score == 2 do
+  player1_roll = roll
+  player2_roll = roll
+  puts "p1: #{player1_roll} | p2: #{player2_roll}"
+  if player1_roll > player2_roll
+    player1_score += 1
+  else
+    player2_score += 1
+  end
 end
+puts "p1: #{player1_score} points | p2: #{player2_score} points"
